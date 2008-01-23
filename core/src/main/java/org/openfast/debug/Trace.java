687d67df1a0ae7dbf9efeb3438ae725db83769f7
jacob_northey <jacob_northey@sourceforge.net>
2008-01-23 22:29:53 +0000

    Added ability to set writer for traces.

--- core/src/main/java/org/openfast/debug/Trace.java
+++ core/src/main/java/org/openfast/debug/Trace.java
@@ -1,5 +1,7 @@
 package org.openfast.debug;
 
+import java.io.PrintWriter;
+
 import org.openfast.FieldValue;
 import org.openfast.template.Field;
 import org.openfast.template.Group;
@@ -9,4 +11,5 @@ public interface Trace {
 	void groupEnd();
 	void field(Field field, FieldValue value, FieldValue encoded, byte[] encoding, int pmapIndex);
 	void pmap(byte[] pmap);
+	void setWriter(PrintWriter writer);
 }
