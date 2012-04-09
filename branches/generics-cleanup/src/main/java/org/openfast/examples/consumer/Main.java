7d2eede0b539a94998669c5d21199b80404baf40
joshseidel <joshseidel@e6ee02d6-d133-0410-bd08-f9337e8129a4>
2012-04-09 18:51:59 +0000

    Merged the branch with the most recent version of trunk.

--- branches/generics-cleanup/src/main/java/org/openfast/examples/consumer/Main.java
+++ branches/generics-cleanup/src/main/java/org/openfast/examples/consumer/Main.java
@@ -26,6 +26,7 @@ public class Main extends OpenFastExample {
         options.addOption("t", MESSAGE_TEMPLATE_FILE, true, "Message template definition file");
         options.addOption("j", READ_OFFSET, true, READ_OFFSET_DESCRIPTION);
         options.addOption("z", VARIANT, true, VARIANT_DESCRIPTION);
+        options.addOption("d", RESET, false, RESET_DESCRIPTION);
     }
     
     /**
@@ -45,10 +46,8 @@ public class Main extends OpenFastExample {
             String host = cl.hasOption(HOST) ? cl.getOptionValue(HOST) : "localhost";
             String ifaddr = cl.hasOption(INTERFACE) ? cl.getOptionValue(INTERFACE) : null;
             
-            if (cl.hasOption(PROTOCOL)) {
-                if ("udp".equals(cl.getOptionValue(PROTOCOL))) {
-                    endpoint = new MulticastClientEndpoint(port, host, ifaddr);
-                }
+            if(isMulticast(cl)) {
+                endpoint = new MulticastClientEndpoint(port, host, ifaddr);
             }
             if (endpoint == null) {
                 endpoint = new TcpEndpoint(host, port);
@@ -65,8 +64,9 @@ public class Main extends OpenFastExample {
 
         final int readOffset = cl.hasOption(READ_OFFSET) ? getInteger(cl, READ_OFFSET) : 0;
 		final Variant variant = cl.hasOption(VARIANT) ? getVariant(cl) : Variant.DEFAULT;
-		final MessageBlockReaderFactory msgBlockReaderFactory = new MessageBlockReaderFactory(variant, readOffset);
-		FastMessageConsumer consumer = new FastMessageConsumer(endpoint, templatesFile, msgBlockReaderFactory);
+        final boolean shouldResetOnEveryMessage = (cl.hasOption(RESET) || (Variant.CME == variant));
+		final MessageBlockReaderFactory msgBlockReaderFactory = new MessageBlockReaderFactory(variant, readOffset, isMulticast(cl));
+		FastMessageConsumer consumer = new FastMessageConsumer(endpoint, templatesFile, msgBlockReaderFactory, shouldResetOnEveryMessage);
         
         try {
             consumer.start();
--- branches/generics-cleanup/src/main/java/org/openfast/examples/producer/Main.java
+++ branches/generics-cleanup/src/main/java/org/openfast/examples/producer/Main.java
@@ -26,17 +26,9 @@ public class Main extends OpenFastExample {
         options.addOption("x", XML_DATA_FILE, true, "The XML data to convert to FAST");
         options.addOption("k", WRITE_OFFSET, true, WRITE_OFFSET_DESCRIPTION);
         options.addOption("z", VARIANT, true, VARIANT_DESCRIPTION);
+        options.addOption("d", RESET, false, RESET_DESCRIPTION);
     }
-
-    public static boolean isMulticast(CommandLine cl) {
-        if (cl.hasOption(PROTOCOL)) {
-            if ("udp".equals(cl.getOptionValue(PROTOCOL))) {
-                return true;
-            }
-        }
-        return false;
-    }
-    
+   
     /**
      * @param args
      */
@@ -77,11 +69,12 @@ public class Main extends OpenFastExample {
         try {
 			final int writeOffset = cl.hasOption(WRITE_OFFSET) ? getInteger(cl, WRITE_OFFSET) : 0;
 			final Variant variant = cl.hasOption(VARIANT) ? getVariant(cl) : Variant.DEFAULT;
-			final MessageBlockWriterFactory msgBlockWriterFactory = new MessageBlockWriterFactory(variant, writeOffset);
+			final boolean shouldResetOnEveryMessage = (cl.hasOption(RESET) || (Variant.CME == variant));
+			final MessageBlockWriterFactory msgBlockWriterFactory = new MessageBlockWriterFactory(variant, writeOffset, isMulticast(cl));
 			
 			FastMessageProducer producer = isMulticast(cl)
-                ? new MulticastFastMessageProducer(endpoint, templatesFile, msgBlockWriterFactory)
-                : new FastMessageProducer(endpoint, templatesFile, msgBlockWriterFactory);
+                ? new MulticastFastMessageProducer(endpoint, templatesFile, msgBlockWriterFactory, shouldResetOnEveryMessage)
+                : new FastMessageProducer(endpoint, templatesFile, msgBlockWriterFactory, shouldResetOnEveryMessage);
 
             producer.start();
             producer.encode(xmlDataFile);
